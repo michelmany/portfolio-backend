b0a840e750604bcee8c9e2404ba4936586c6f6a2
michelmany <hello@michelmany.com>
2025-04-17 18:55:27 +0200

    fix: user type issue

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export interface AuthenticatedRequest extends Request {
         id: string;
         email: string;
         name: string;
-        bio?: string;
-        avatar?: string;
+        bio?: string | undefined;
+        avatar?: string | undefined;
     };
 }
